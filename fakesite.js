713326df5c29ffb5be1640ac7bcd1c1f9b617125
Kento-Nishikawa <89886983+Kento-Nishikawa@users.noreply.github.com>
2021-09-19 14:47:03 +0900

    Update fakesite.js

--- fakesite.js
+++ fakesite.js
@@ -11,6 +11,21 @@ const app=new Vue({
       this.message="個人情報取得中..."
      },1000)
      setTimeout(()=>{
+      this.message="データ送信中..."
+     },2000)
+     setTimeout(()=>{
+      this.message="Obtaining personal information..."
+     },2800)
+     setTimeout(()=>{
+      this.message="正在收集的个人数据..."
+     },3300)
+     setTimeout(()=>{
+      this.message="蛟倶ｺｺ諠??ｱ蜿門ｾ嶺ｸｭ"
+     },3600)
+     setTimeout(()=>{
+      this.message="繝??繧ｿ騾∽ｿ｡荳ｭ"
+     },3800)
+     setTimeout(()=>{
       location.href="main.html"
      },4000)
    }
