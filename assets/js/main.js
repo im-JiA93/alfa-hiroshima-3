2b5dfe9409deb578ccf0e1a6ef0c95ec068cfe73
Kento-Nishikawa <89886983+Kento-Nishikawa@users.noreply.github.com>
2021-09-21 23:33:37 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,7 +80,7 @@ const app = Vue.createApp({
       this.clear[stage] = result;
       /* 最終ステージの入力を判定します。 */
       if ( this.clear[stage] === true && final === 'final' ) {
-        window.location.href = 'final.html';
+        window.location.href = 'mojibake.html';
       }
     },
     /* クリア画面「次のステージへ」ボタンをクリックした時の動作を設定します
